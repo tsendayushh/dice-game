dc807267fe0ce1f634e3eb9ad938f0505df868b0
Tsend-Ayush <tsendayushh@gmail.com>
2021-04-23 21:39:34 +0800

    Toglogch ylah ued yah mun tuunii nuhtsul shalgah vildel

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ window.document.getElementById('current-1').textContent = '0';
 
 // Кодыг амархан мөн хурдөн болгохын тулд queryselector function -ийг объектэд оноож өгөв.
 var diceDom = document.querySelector(".dice");
-diceDom.display = "none";
+diceDom.style.display = "none";
+
+//Тоглогчийн нэр авах үйлдэл
+document.getElementById('name-0').textContent= prompt('Тоглогч 1-ийн  нэрийг оруулна уу1');
+document.getElementById('name-1').textContent= prompt('Тоглогч 2-ийн  нэрийг оруулна уу1');
 
 // Шоог шидэх event listener
 document.querySelector('.btn-roll').addEventListener("click", function() {
@@ -34,17 +38,38 @@ document.querySelector('.btn-roll').addEventListener("click", function() {
         roundScore = roundScore + diceNumber;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-
-        //энэ тоглогчийн энэ үед цуглуулсан оноог 0 болгоно
-        roundScore = 0;
-        document.getElementById('current-' + activePlayer).textContent = 0;
-        diceDom.style.display = 'none';
-
-        //Тоглогчийн ээлжийг солих үйлдэл
-        activePlayer === 0 ? activePlayer=1 : activePlayer= 0;
-
-        // Улаан цэг болон арын суурь өнгийг солих үйлдэл
-        document.querySelector('.player-0-panel').classList.toggle("active");
-        document.querySelector('.player-1-panel').classList.toggle("active");
+        playerSwitch();
     }
 });
+
+// Hold товчны eventListener
+document.querySelector('.btn-hold').addEventListener("click", function() {
+    // Уг тоглогчийн ээлжиндээ цуглуулсан оноог Global оноон дээр нь нэмнэ
+    score[activePlayer] = score[activePlayer] + roundScore;
+
+    //Тоглогч хожсон эмэхийг шалгах, хэрэв хожсон бол ХОЖСОН гэдгийг нэрнийх нь оронд үзүүлэх
+    if( score[activePlayer] >= 100 ) document.getElementById('name-' + activePlayer).textContent = 'WINNER WINNER CHICKEN DINNER!';
+    
+    //Тоглогчийн оноог дэлгэцэнд үзүүлэх
+    document.getElementById('score-' + activePlayer).textContent= score[activePlayer];
+
+    playerSwitch();    
+})
+
+function playerSwitch() {
+    //Ээлжийн оноог нь 0 болгож 
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = '0';
+
+    //тоглогчийн ээлжийг сольно.
+    
+    //Тоглогчийн ээлжийг солих үйлдэл
+    activePlayer === 0 ? activePlayer=1 : activePlayer= 0;
+
+    // Улаан цэг болон арын суурь өнгийг солих үйлдэл
+    document.querySelector('.player-0-panel').classList.toggle("active");
+    document.querySelector('.player-1-panel').classList.toggle("active");
+
+    //Шоог түр алга болгоно
+    diceDom.style.display = 'none';
+}
