72dbd151267ae3392e91a77ffd66ef1ac075d2c4
Tsend-Ayush <tsendayushh@gmail.com>
2021-04-23 19:14:17 +0800

    Roll dice button darahad shoog hayj tvvnii zurgiig gargah eventhandler hiisen

--- app.js
+++ app.js
@@ -8,20 +8,26 @@ var score = [0,0];
 var roundScore = 0;
 
 //Шоог хаяахад хэд буухыг хадгалах хувьсагч, 1-6 гэсэн утгыг санамсаргүйгээр оноох
-var dice = Math.floor(Math.random()*6) + 1;
+// var diceNumber = Math.floor(Math.random()*6) + 1;
 
-console.log('Шоо : '+ dice + ' гэж буулаа.'); // Буусан шооны тоо
+//console.log('Шоо : '+ diceNumber + ' гэж буулаа.'); // Буусан шооны тоо
 
-document.querySelector('.dice').style.display = "none";
 
 /* Тоглогчийн энэ үе дэх оноог 0 болгов.
 <div class="player-score" id="score-0">43</div>
 <div class="player-score" id="score-1">72</div>*/
-window.document.querySelector('#score-0').textContent = 0;
-window.document.querySelector('#score-1').textContent = 0;
+window.document.getElementById('score-0').textContent = '0';
+window.document.getElementById('score-1').textContent = '0';
 
 //Тоглогчийн нийт цуглуулсан оноог 0 болгов.
-window.document.querySelector('#current-0').textContent = 0;
-window.document.querySelector('#current-1').textContent = 0;
+window.document.getElementById('current-0').textContent = '0';
+window.document.getElementById('current-1').textContent = '0';
 
-console.log('Тоглогчийн энэ үеийн оноо '+ window.document.querySelector('#score-0').textContent + ' байна.');
+var diceDom = document.querySelector(".dice");
+
+document.querySelector('.btn-roll').addEventListener("click", function() {
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
+});
